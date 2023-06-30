fa94154e628062815591430dd5ca743cd0eb7ff7
Shucayb <shuib290@gmail.com>
2023-06-30 12:00:52 +0100

    updated code with CI-CD

--- website/index.js
+++ website/index.js
@@ -1,8 +1,63 @@
+$(document).ready(function (e) {
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function () {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function (e) {
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 768) {
+        $navbar.css({ left: `-${width}px` });
+    } else {
+        $navbar.css({ left: '0px' });
+    }
+}
+
+var typed = new Typed('#typed', {
+    strings: [
+        'Cloud Engineer',
+        'DevOps Engineer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2', {
+    strings: [
+        'Cloud Engineer',
+        'DevOps Engineer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://6o3vz2tgvdowsnywrqyicy5dq40mqnfr.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+    counter.innerHTML = `Visits: ${data}`;
 }
 updateCounter();
-
